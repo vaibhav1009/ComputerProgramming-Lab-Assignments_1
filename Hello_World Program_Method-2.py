6d29311339290e2198d38ef3b91143ed9fec43dd
Vaibhav Kagathara <54314204+vaibhav1009@users.noreply.github.com>
2019-09-15 23:53:13 +0530

    Add files via upload
    Hello World Program Using String

---
+++ Hello_World Program_Method-2.py
@@ -0,0 +1,12 @@
+#  ********************************
+#   Author - Vaibhav Kagathara
+#   Date :- 15th September 2019
+#   Language :- Python
+#   About :- Hello World Program
+#  ********************************
+
+
+#Writing 1st Program For Ever Programmer Ever By Using String.!
+
+string_1 = "Hello World"
+print(string_1)
