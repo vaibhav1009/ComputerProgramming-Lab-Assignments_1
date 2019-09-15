4c021c725c3eafa466428e0e2e0c15931eb6edc1
Vaibhav Kagathara <54314204+vaibhav1009@users.noreply.github.com>
2019-09-15 23:56:25 +0530

    Update Hello_World Program_Method-2.py

--- Hello_World Program_Method-2.py
+++ Hello_World Program_Method-2.py
@@ -1,9 +1,9 @@
-#  ********************************
+#  ***********************************************
 #   Author - Vaibhav Kagathara
 #   Date :- 15th September 2019
 #   Language :- Python
-#   About :- Hello World
-#  ********************************
+#   About :- Printing Hello World
+#  ***********************************************
 
 
 #Writing 1st Program For Ever Programmer Ever By Using String.!
