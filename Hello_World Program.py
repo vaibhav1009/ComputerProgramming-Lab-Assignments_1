f4b7b20d8e89f5adeae2be943218d2487e13263e
Vaibhav Kagathara <54314204+vaibhav1009@users.noreply.github.com>
2019-09-15 23:01:18 +0530

    Update Hello_World Program.py

--- Hello_World Program.py
+++ Hello_World Program.py
@@ -2,7 +2,7 @@
 #   Author - Vaibhav Kagathara
 #   Date :- 15th September 2019
 #   Language :- Python
-#   About :- Hello World Program
+#   About :- Hello World
 #  ********************************
 
 
