650a981d59811db3532c88979021f6fe09ada9af
Vaibhav Kagathara <54314204+vaibhav1009@users.noreply.github.com>
2019-09-18 15:56:46 +0530

    Update Writing Cindition Using if Statement to Compare the Value.py

--- Writing Cindition Using if Statement to Compare the Value.py
+++ Writing Cindition Using if Statement to Compare the Value.py
@@ -2,7 +2,7 @@
 #   Author - Vaibhav Kagathara
 #   Date :- 18th September 2019
 #   Language :- Python
-#   About :- Writing Cindition Using if Statement to Check the Value.py
+#   About :- Writing Cindition Using if Statement to Compare the Value
 #  **********************************************************************
 
 
