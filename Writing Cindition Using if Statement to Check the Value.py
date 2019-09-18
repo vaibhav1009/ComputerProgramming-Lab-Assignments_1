08fdcea3cf99aca6103075c4df4a01d7c34df55c
Vaibhav Kagathara <54314204+vaibhav1009@users.noreply.github.com>
2019-09-18 15:51:41 +0530

    Add files via upload

---
+++ Writing Cindition Using if Statement to Check the Value.py
@@ -0,0 +1,12 @@
+#  *****************************************************
+#   Author - Vaibhav Kagathara
+#   Date :- 15th September 2019
+#   Language :- Python
+#   About :- Printing Custom Statement Using Varible
+#  *****************************************************
+
+
+a=10
+
+if a==10:
+    print("The Value Of a is 10.")
