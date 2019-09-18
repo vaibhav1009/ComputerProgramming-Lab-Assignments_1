f1511c62874aaddc89ed552f695e965b6ad9442a
Vaibhav Kagathara <54314204+vaibhav1009@users.noreply.github.com>
2019-09-18 15:56:11 +0530

    Add files via upload

---
+++ Writing Cindition Using if Statement to Compare the Value.py
@@ -0,0 +1,11 @@
+#  **********************************************************************
+#   Author - Vaibhav Kagathara
+#   Date :- 18th September 2019
+#   Language :- Python
+#   About :- Writing Cindition Using if Statement to Check the Value.py
+#  **********************************************************************
+
+
+
+if 20>10:
+    print("20 is greater than 10.")
