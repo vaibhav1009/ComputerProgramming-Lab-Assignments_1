98d36995e53bb8a90e3133169faa0fc61ded1979
Vaibhav Kagathara <54314204+vaibhav1009@users.noreply.github.com>
2019-09-18 16:03:33 +0530

    Add files via upload

---
+++ Printing Fruit List.py
@@ -0,0 +1,10 @@
+#  *****************************************************
+#   Author - Vaibhav Kagathara
+#   Date :- 18th September 2019
+#   Language :- Python
+#   About :- Printing Fruit List
+#  *****************************************************
+
+
+fruitlist = ["Mango", "Banana", "Apple", "Orange","Grapes"]
+print(fruitlist)
