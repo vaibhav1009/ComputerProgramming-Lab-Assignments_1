6f3a8cb79f2134fafc88215316c7ccd7a3900c49
Vaibhav Kagathara <54314204+vaibhav1009@users.noreply.github.com>
2019-09-18 16:08:23 +0530

    Add files via upload

---
+++ If-Else Condition To Compare 2 Numbers.py
@@ -0,0 +1,14 @@
+#  **********************************************************************
+#   Author - Vaibhav Kagathara
+#   Date :- 18th September 2019
+#   Language :- Python
+#   About :- If-Else Condition To Compare 2 Numbers
+#  **********************************************************************
+
+
+a = 10
+b = 20
+if b > a:
+  print("b is greater than a.")
+else:
+  print("a is greater than b.")
